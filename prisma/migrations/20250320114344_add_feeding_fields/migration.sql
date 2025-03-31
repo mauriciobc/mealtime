615a3a19babd4eed8ceabfda923c9e02c6d51e5a
Mauricio Castro <mauriciobarbosaecastro@outlook.com>
2025-03-30 21:14:29 -0300

    Atualiza a configuração do banco de dados para utilizar PostgreSQL em vez de SQLite. Adiciona o campo 'ownedCats' ao modelo User e altera a relação do modelo Cat para referenciar o novo campo 'owner'. Remove migrações antigas relacionadas ao SQLite e atualiza as dependências do Prisma no package.json e package-lock.json.

--- prisma/migrations/20250320114344_add_feeding_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Cat" ADD COLUMN "feeding_interval" INTEGER DEFAULT 8;
-ALTER TABLE "Cat" ADD COLUMN "portion_size" REAL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "FeedingLog" ADD COLUMN "status" TEXT DEFAULT 'completed';
